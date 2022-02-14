02d03ee4d6e938685de1ca20efdc13e353d5573d
MatousAc <matoush@southern.edu>
2022-02-13 22:30:54 -0500

    module Czech

---
+++ sauSoftSkills/moduleCzech.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+
+df = pd.read_csv('sauFinal.csv')
+df["test"] = random.random()  #1 if ("I") else 2 if ("R") else 3 if ("M") else 0
+print(df.head(10))
+print(df.columns)
+
+# dataPoints = pd.crosstab(index = df["deptCode"], columns="count")
+sns.barplot(x="deptCode", y="minCredit", data=df, estimator=sum)
+plt.show()
